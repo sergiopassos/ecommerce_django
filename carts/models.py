e7bbd20560fb56e417edb9f25a604928df93056d
sergiopassos <sergio.passos@users.noreply.github.com>
2019-04-11 19:44:54 -0300

    Após testado na view, mudamos a regra de negocio para a model, onde tudo deve estar

--- carts/models.py
+++ carts/models.py
@@ -6,6 +6,22 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
+	def new_or_get(self, request):
+		cart_id = request.session.get("cart_id", None)
+		qs = self.get_queryset().filter(id=cart_id)
+		if qs.count() == 1:
+			new_obj = False
+			cart_obj = qs.first()
+			if request.user.is_authenticated() and cart_obj.user is None:
+				cart_obj.user = request.user
+				cart_obj.save()
+		else:
+			cart_obj = Cart.objects.new(user=request.user)
+			new_obj = True
+			request.session['cart_id'] = cart_obj.id
+		return cart_obj, new_obj
+
+
 	def new(self, user=None):
 		print(user)
 		user_obj = None
@@ -22,6 +38,6 @@ class Cart(models.Model):
 	timestamp	= models.DateTimeField(auto_now_add=True)
 
 	objects = CartManager()
-	
+
 	def __str__(self):
 		return str(self.id)
