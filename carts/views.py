7f7a65661e720e3a20e3e7064bc4992899f43892
sergiopassos <sergio.passos02@gmail.com>
2019-05-12 14:31:51 -0300

    Refactoring view to models, created Billing Manager.

--- carts/views.py
+++ carts/views.py
@@ -42,21 +42,11 @@ def checkout_home(request):
 	order_obj = None
 	if cart_created or cart_obj.products.count() == 0:
 		return redirect("cart:home")
-	user = request.user
-	billing_profile = None
+
 	login_form = LoginForm()
 	guest_form = GuestForm()
-	guest_email_id = request.session.get('guest_email_id')
-	if user.is_authenticated():
-		billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
-														user=user, email=user.email)
-	elif guest_email_id is not None:
-		guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
-		billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(
-											email=guest_email_obj.email)
-	else:
-		pass
 
+	billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
 	if billing_profile is not None:
 		order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
 
@@ -64,7 +54,7 @@ def checkout_home(request):
 		"object": order_obj,
 		"billing_profile": billing_profile,
 		"login_form": login_form,
-		"guest_form": guest_form,
+		"guest_form": guest_form
 
 	}
 	return render(request, "carts/checkout.html", context)
