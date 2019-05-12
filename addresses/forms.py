8adb0348b84c41de4ffc40b35ab6eb464ac15750
sergiopassos <sergio.passos02@gmail.com>
2019-05-12 20:16:43 -0300

    Criamos o formulário usando os campos da da model e adicionamos o form no checkout.

--- addresses/forms.py
+++ addresses/forms.py
@@ -5,3 +5,13 @@ from .models import Address
 class AddressForm(forms.ModelForm):
 	class Meta:
 		model = Address
+		fields = [
+			'billing_profile',
+			'address_type',
+			'address_line_1',
+			'address_line_2',
+			'city',
+			'country',
+			'postal_code',
+
+		]
