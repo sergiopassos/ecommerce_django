cc055ee2bfd8b4d6f62f0a9d641faaf4f51ddba0
sergiopassos <sergio.passos02@gmail.com>
2019-05-15 20:18:09 -0300

    Normalização pep8

--- carts/views.py
+++ carts/views.py
@@ -17,60 +17,64 @@ from .models import Cart
 # 	return render(request, "carts/home.html", {})
 
 def cart_home(request):
-	cart_obj, new_obj = Cart.objects.new_or_get(request)
-	return render(request, "carts/home.html", {"cart": cart_obj})
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {"cart": cart_obj})
+
 
 def cart_update(request):
-	print(request.POST)
-	product_id = request.POST.get('product_id')
-	if product_id is not None:
-		try:
-			product_obj = Product.objects.get(id=product_id)
-		except Product.DoesNotExist:
-			print("Show message to user, products is gone?")
-			return redirect("cart:home")
-		cart_obj, new_obj = Cart.objects.new_or_get(request)
-		if product_obj in cart_obj.products.all():
-			cart_obj.products.remove(product_obj)
-		else:
-			cart_obj.products.add(product_obj) #  cart_obj.products.add(product_id)
-
-		request.session['cart_items'] = cart_obj.products.count()
-	
-		# return redirect(product_obj.get_absolute_url())
-	return redirect("cart:home")
+    print(request.POST)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print("Show message to user, products is gone?")
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            # cart_obj.products.add(product_id)
+            cart_obj.products.add(product_obj)
+        request.session['cart_items'] = cart_obj.products.count()
+        # return redirect(product_obj.get_absolute_url())
+    return redirect("cart:home")
 
 
 def checkout_home(request):
-	cart_obj, cart_created = Cart.objects.new_or_get(request)
-	order_obj = None
-	if cart_created or cart_obj.products.count() == 0:
-		return redirect("cart:home")
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
 
-	login_form = LoginForm()
-	guest_form = GuestForm()
-	address_form = AddressForm()
-	billing_address_id = request.session.get("billing_address_id", None)
-	shipping_address_id = request.session.get("shipping_address_id", None)
+    login_form = LoginForm()
+    guest_form = GuestForm()
+    address_form = AddressForm()
+    billing_address_id = request.session.get("billing_address_id", None)
+    shipping_address_id = request.session.get("shipping_address_id", None)
 
-	billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-	if billing_profile is not None:
-		order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
-		if shipping_address_id:
-			order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
-			del request.session["shipping_address_id"]
-		if billing_address_id:
-			order_obj.billing_address = Address.objects.get(id=billing_address_id)
-			del request.session["billing_address_id"]
-		if billing_address_id or shipping_address_id:
-			order_obj.save()
+    billing_profile, billing_profile_created = (
+        BillingProfile.objects.new_or_get(request))
+    if billing_profile is not None:
+        order_obj, order_obj_created = (
+            Order.objects.new_or_get(billing_profile, cart_obj))
+        if shipping_address_id:
+            order_obj.shipping_address = (
+                Address.objects.get(id=shipping_address_id))
+            del request.session["shipping_address_id"]
+        if billing_address_id:
+            order_obj.billing_address = (
+                Address.objects.get(id=billing_address_id))
+            del request.session["billing_address_id"]
+        if billing_address_id or shipping_address_id:
+            order_obj.save()
 
-	context = {
-		"object": order_obj,
-		"billing_profile": billing_profile,
-		"login_form": login_form,
-		"guest_form": guest_form,
-		"address_form": address_form,
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form,
+        "guest_form": guest_form,
+        "address_form": address_form,
 
-	}
-	return render(request, "carts/checkout.html", context)
+    }
+    return render(request, "carts/checkout.html", context)
