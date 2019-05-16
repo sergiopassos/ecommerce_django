82a57c9ec7b317e7d788323d0d3b95107d2efc24
sergiopassos <sergio.passos02@gmail.com>
2019-05-15 21:10:52 -0300

    Processo de checkout finalizado

--- carts/views.py
+++ carts/views.py
@@ -68,6 +68,15 @@ def checkout_home(request):
             del request.session["billing_address_id"]
         if billing_address_id or shipping_address_id:
             order_obj.save()
+    
+    if request.method == "POST":
+        "check that order is done"
+        is_done = order_obj.check_done()
+        if is_done:
+            order_obj.mark_paid()
+            request.session['cart_items'] = 0
+            del request.session['cart_id']
+            return redirect("/cart/success")
 
     context = {
         "object": order_obj,
